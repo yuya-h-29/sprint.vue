4c9953fd36c285410a224ca3cb29160158c6b629
yuya-h-29 <tzou11291996@gmail.com>
2020-05-05 07:35:03 +0900

    created function for images

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <h1>{{}}</h1>
-
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <img ：src="photos" alt="" srcset="" /> -->
+    <!-- <h1>{{ func1() }}</h1> -->
+    <!-- <h1>{{ photos }}</h1> -->
+    <!-- <div v-for="n in 100" :key="item.message"> -->
+    <!-- <img src="" alt="" srcset=""> -->
+    <!-- </div> -->
     <navbar />
     <allPhotos />
     <singlePhoto />
@@ -13,9 +17,9 @@
 import Navbar from "./components/Navbar";
 import AllPhotos from "./components/AllPhotos";
 import SinglePhoto from "./components/SinglePhoto";
-import { listObjects} from "../utils/index";
+import { listObjects, getSingleObject } from "../utils/index";
 
-//  getSingleObject, saveObject
+//  saveObject
 
 export default {
   name: "App",
@@ -25,19 +29,29 @@ export default {
     singlePhoto: SinglePhoto,
   },
   data: () => ({
-    photo: [],
+    photos: [],
+    currentView: "all",
+    selectedPhoto: "none",
   }),
   methods: {
-      func1:function name(params) {
-
-      },
-  //     selectOnePhoto(el) {
-  // console.log("something else")
-  //     }
-
-
-  //
-  //   }
+    func1: async function getAllPictures() {
+      const arrOfPromisePictures = await listObjects();
+      // console.log(arrOfPromisePictures);
+      const arrOrPicFiles = arrOfPromisePictures.map((obj) => {
+        return obj.Key;
+      });
+      const changeToBase64 = Promise.all(
+        arrOrPicFiles.map((fileName) => {
+          return getSingleObject(fileName);
+        })
+      ).then((result) => {
+        result.map((pictureData) => {
+          return "data:image/jpg;base64," + pictureData;
+        });
+      });
+      return this.photos.push(changeToBase64);
+    },
+  },
 };
 </script>
 
